6f06831294d5b8afbc2a916ea9656aecea00eee2
FlavioPatti <s301104@studenti.polito.it>
2023-05-10 11:01:47 +0200

    .

--- pytorch_benchmarks/hr_detection/model.py
+++ pytorch_benchmarks/hr_detection/model.py
@@ -25,15 +25,15 @@ def get_reference_model(model_name: str, model_config: Optional[Dict[str, Any]] 
     if model_name == 'vit_freq+time_finetune':
         print(f"ViT Freq+Time Finetune")
         return MaskedAutoencoderViT_without_decoder(
-        img_size = 256, in_chans = 4, mask_2d=True, typeExp = "freq+time",
-        patch_size=16, embed_dim=64, depth=12, num_heads=16,
-        decoder_embed_dim=64, decoder_num_heads=16,
+        img_size = (64,256), in_chans = 4, mask_2d=True, typeExp = "freq+time",
+        patch_size=8, embed_dim=256, depth=12, num_heads=16,
+        decoder_embed_dim=256, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6) )
     if model_name == 'vit_time_finetune':
         print(f"ViT Time Finetune")
         return MaskedAutoencoderViT_without_decoder(
         img_size = 256, in_chans = 4, mask_2d=False, typeExp = "time",
-        patch_size=4, embed_dim=64, depth=12, num_heads=16,
+        patch_size=1, embed_dim=64, depth=12, num_heads=16,
         decoder_embed_dim=64, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6) )
             
