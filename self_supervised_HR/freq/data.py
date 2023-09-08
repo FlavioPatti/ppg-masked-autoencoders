2282aec95e8a6e20479fc9ba26e06c3e8aaffdf3
FlavioPatti <s301104@studenti.polito.it>
2023-09-08 13:11:57 +0200

    .

--- self_supervised_HR/freq/data.py
+++ self_supervised_HR/freq/data.py
@@ -60,9 +60,6 @@ def _collect_data(data_dir, data):
     elif data == "DALIA":
       folder = "PPG_FieldStudy"
       num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    elif data == "IEEETRAIN":
-      num = [1,2,3,4,5,6,7,8,9,10,11,12]
-      ty = [1,2,2,2,2,2,2,2,2,2,2,2]
 
     dataset = dict()
     session_list = random.sample(num, len(num))
@@ -114,62 +111,6 @@ def _collect_data(data_dir, data):
           'acc': acc,
           'target': target
               }
-        
-    elif data == "IEEETRAIN":
-        for idx, subj in enumerate (num):
-            if subj <= 9:
-              sub = '0'+str(subj)
-              t = '0'+str(ty[idx])
-              data = scipy.io.loadmat(f'{data_dir}/DATA_{sub}_TYPE{t}.mat')['sig']
-            else:
-              t = '0'+str(ty[idx])
-              data = scipy.io.loadmat(f'{data_dir}/DATA_{subj}_TYPE{t}.mat')['sig']
-            #la prima riga = ECG signals
-            ecg_signal = data[0:1, :]
-            ecg_signal = np.squeeze(ecg_signal)
-            #seconda e terza riga = PPG signals
-            ppg = data[1:3, :]
-            ppg = np.transpose(ppg, (1, 0))
-            # ultime tre righe = acc signals
-            acc = data[3:6, :]
-            acc = np.transpose(acc, (1, 0))
-            
-            fs = 125
-            _, results = neurokit2.ecg_peaks(ecg_signal, sampling_rate=fs)
-            rpeaks = results["ECG_R_Peaks"]
-
-            #Correct peaks
-            rpeaks = wfdb.processing.correct_peaks(
-            ecg_signal, rpeaks, search_radius=20, smooth_window_size=50, peak_dir="up")
-            #print(f"shape peaks = {rpeaks.shape}")
-            #print(f"rpeaks = {rpeaks}")
-
-            intervalli_tempo = [rpeaks[i+1] - rpeaks[i] for i in range(len(rpeaks)-1)]
-            instant_heart_rate = [60 / (intervallo_tempo / fs) for intervallo_tempo in intervalli_tempo]
-            window_size = 8.0 * fs 
-            shift = 2.0 * fs  
-            
-
-            heart_rate_mean = []
-            numero_iterazioni = int((ecg_signal.shape[0] - window_size) // shift + 1)
-            for j in range(0, numero_iterazioni):
-                heart_rate_current_window = []
-                inizio_finestra = j * shift
-                fine_finestra = inizio_finestra + window_size
-                for i in range(0, len(rpeaks)-1):
-                    if rpeaks[i] >= inizio_finestra and rpeaks[i] < fine_finestra:
-                        heart_rate_current_window.append(instant_heart_rate[i])
-                heart_rate_mean.append(mean(heart_rate_current_window))
-            
-            target = np.array(heart_rate_mean).astype('float32')
-    
-            dataset[subj] = { 
-            #each sample is build by: ppg value, accelerometer value, hr estimation
-            'ppg': ppg,
-            'acc': acc,
-            'target': target
-                }
-    
     return dataset
 
 
@@ -178,10 +119,8 @@ def _preprocess_data(data_dir, dataset, dataset_name):
     """
     Process data with a sliding window of size 'time_window' and overlap 'overlap'
     """
-    if dataset_name == "IEEETRAIN":
-      fs = 125
-    else:
-      fs = 32
+    
+    fs = 32
       
     time_window = 8
     overlap = 2
@@ -260,52 +199,6 @@ def _get_data_gen_dalia_wesad(samples, targets, groups, data_dir, AUGMENT, datas
         yield ds_train, ds_val, ds_test
         kfold_it += 1
 
-def _get_data_gen_ieee(samples, targets, groups):
-    subjects = 12 #number of patients on which PPG data is taken
-    n = 3 #number of folds 
-    
-    indices, _ = _rndgroup_kfold(groups, n)
-    kfold_it = 0
-    while kfold_it < subjects:
-        fold = kfold_it // (n+1)
-        print(f'kFold-iteration: {kfold_it}')
-        train_index, test_val_index = indices[fold]
-        # Train Dataset
-        train_samples = samples[train_index]
-        train_targets = targets[train_index] #target = hr estimation
-        ds_train = Dalia(train_samples, train_targets)
-        # Val and Test Dataset
-        logo = LeaveOneGroupOut()
-        samples_val_test = samples[test_val_index]
-        targets_val_test = targets[test_val_index]
-        groups_val_test = groups[test_val_index]
-        j = 0
-        for val_index, test_index in logo.split(samples_val_test,
-                                                targets_val_test,
-                                                groups_val_test):
-
-            if j == kfold_it % (n+1):
-                val_samples = samples_val_test[val_index]
-                val_targets = targets_val_test[val_index]
-                ds_val = Dalia(val_samples, val_targets)
-                test_subj = groups[test_val_index][test_index][0]
-                print(f'Test Subject: {test_subj}')
-                print(f"Test & Val Subjects: {np.unique(groups[test_val_index])}")
-                test_samples = samples_val_test[test_index]
-                test_targets = targets_val_test[test_index]
-                
-                ds_test = Dalia(test_samples, test_targets, test_subj) 
-               #the dataset is unzipped in several 'Sx' files, each with a different number of samples 
-               #for the different activities (that are the same for all the subjects)
-               #we use k-fold to train the TempoNet several Sx during training and then test 
-               #on a different and unseen Sx at each iteration
-                
-            j += 1
-
-        yield ds_train, ds_val, ds_test
-        kfold_it += 1
-
-
 def _rndgroup_kfold(groups, n, seed=35):
     """
     Random analogous of sklearn.model_selection.GroupKFold.split.
@@ -360,30 +253,22 @@ def get_data(dataset_name = "WESAD",data_dir=None,url=WESAD_URL,ds_name='ppg_dal
       folder = "PPG_FieldStudy"
       url = DALIA_URL
     
-    if dataset_name == "DALIA" or dataset_name == "WESAD":
-      if data_dir is None:
-          data_dir = Path('.').absolute() / dataset_name
-          print(f"data dir = {data_dir}")
-      filename = data_dir / ds_name
-      # Download if does not exist
-      if not filename.exists():
-          print('Download in progress... Please wait.')
-          ds_dalia = requests.get(url)
-          data_dir.mkdir()
-          with open(filename, 'wb') as f:
-              f.write(ds_dalia.content)
-      # Unzip if needed
-      if not (data_dir / folder).exists():
-          print('Unzip files... Please wait.')
-          with zipfile.ZipFile(filename) as zf:
-              zf.extractall(data_dir)
-    if dataset_name == "IEEETRAIN":
-        data_dir = Path('.').absolute() / dataset_name / 'Training_data'
-        # set data folder, train & test
-        data_folder = "./IEEETRAIN/"
-        train_file = data_folder + "competition_data.zip"
-        with zipfile.ZipFile(train_file) as zf:
-          zf.extractall(data_folder)
+    if data_dir is None:
+        data_dir = Path('.').absolute() / dataset_name
+        print(f"data dir = {data_dir}")
+    filename = data_dir / ds_name
+    # Download if does not exist
+    if not filename.exists():
+        print('Download in progress... Please wait.')
+        ds_dalia = requests.get(url)
+        data_dir.mkdir()
+        with open(filename, 'wb') as f:
+            f.write(ds_dalia.content)
+    # Unzip if needed
+    if not (data_dir / folder).exists():
+        print('Unzip files... Please wait.')
+        with zipfile.ZipFile(filename) as zf:
+            zf.extractall(data_dir)
 
     # This step slims the dataset. This will help to speedup following usage of data
     if not (data_dir / 'slimmed_dalia.pkl').exists():
@@ -393,11 +278,9 @@ def get_data(dataset_name = "WESAD",data_dir=None,url=WESAD_URL,ds_name='ppg_dal
         with open(data_dir / 'slimmed_dalia.pkl', 'rb') as f:
             dataset = pickle.load(f, encoding='latin1')
         samples, target, groups = dataset.values()
+   
+    generator = _get_data_gen_dalia_wesad(samples, target, groups, data_dir = data_dir, AUGMENT = augment, dataset_name = dataset_name)
     
-    if dataset_name == "DALIA" or dataset_name == "WESAD":
-        generator = _get_data_gen_dalia_wesad(samples, target, groups, data_dir = data_dir, AUGMENT = augment, dataset_name = dataset_name)
-    if dataset_name == "IEEETRAIN":
-        generator = _get_data_gen_ieee(samples, target, groups)
     return generator 
 
 def get_full_dataset(dataset_name,  data_dir=None, url=WESAD_URL, ds_name='ppg_dalia.zip'):
@@ -408,31 +291,23 @@ def get_full_dataset(dataset_name,  data_dir=None, url=WESAD_URL, ds_name='ppg_d
     elif dataset_name == "DALIA":
       folder = "PPG_FieldStudy"
       url = DALIA_URL
-      
-    if dataset_name == "DALIA" or dataset_name == "WESAD":  
-        if data_dir is None:
-            data_dir = Path('.').absolute() / dataset_name
-            print(f"data dir = {data_dir}")
-        filename = data_dir / ds_name
-        # Download if does not exist
-        if not filename.exists():
-            print('Download in progress... Please wait.')
-            ds_dalia = requests.get(url)
-            data_dir.mkdir()
-            with open(filename, 'wb') as f:
-                f.write(ds_dalia.content)
-        # Unzip if needed
-        if not (data_dir / folder).exists():
-            print('Unzip files... Please wait.')
-            with zipfile.ZipFile(filename) as zf:
-                zf.extractall(data_dir)
-    if dataset_name == "IEEETRAIN":
-        data_dir = Path('.').absolute() / dataset_name / 'Training_data'
-        # set data folder, train & test
-        data_folder = "./IEEETRAIN/"
-        train_file = data_folder + "competition_data.zip"
-        with zipfile.ZipFile(train_file) as zf:
-          zf.extractall(data_folder)
+        
+    if data_dir is None:
+        data_dir = Path('.').absolute() / dataset_name
+        print(f"data dir = {data_dir}")
+    filename = data_dir / ds_name
+    # Download if does not exist
+    if not filename.exists():
+        print('Download in progress... Please wait.')
+        ds_dalia = requests.get(url)
+        data_dir.mkdir()
+        with open(filename, 'wb') as f:
+            f.write(ds_dalia.content)
+    # Unzip if needed
+    if not (data_dir / folder).exists():
+        print('Unzip files... Please wait.')
+        with zipfile.ZipFile(filename) as zf:
+            zf.extractall(data_dir)
                 
     # This step slims the dataset. This will help to speedup following usage of data
     if not (data_dir / 'slimmed_dalia.pkl').exists():
